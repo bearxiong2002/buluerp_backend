de724a3f1ad23d28bf17294417b321f490efa1d6
vvcoder <mzp.2005@qq.com>
2025-05-07 23:37:46 +0800

    物料资料CRUD和Excel相关

---
+++ buluerp_vue/ruoyi-admin/src/main/java/com/ruoyi/web/controller/erp/ErpMaterialInfoController.java
@@ -0,0 +1,90 @@
+package com.ruoyi.web.controller.erp;
+
+import com.ruoyi.common.annotation.Anonymous;
+import com.ruoyi.common.annotation.Log;
+import com.ruoyi.common.core.controller.BaseController;
+import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.page.TableDataInfo;
+import com.ruoyi.common.enums.BusinessType;
+import com.ruoyi.common.utils.poi.ExcelUtil;
+import com.ruoyi.web.domain.ErpCustomers;
+import com.ruoyi.web.domain.ErpMaterialInfo;
+import com.ruoyi.web.domain.ErpOrders;
+import com.ruoyi.web.service.IErpMaterialInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/material-info")
+@Api(value = "物料信息相关")
+public class ErpMaterialInfoController extends BaseController {
+    @Autowired
+    private IErpMaterialInfoService erpMaterialInfoService;
+
+    // @PreAuthorize("@ss.hasPermi('system:material-info:list')")
+    @Anonymous
+    @GetMapping("/list")
+    @ApiOperation(value = "获取物料信息列表", notes = "获取物料信息列表")
+    public TableDataInfo list(ErpMaterialInfo erpMaterialInfo) {
+        startPage();
+        List<ErpMaterialInfo> tableDataInfos = erpMaterialInfoService
+                .selectErpMaterialInfoList(erpMaterialInfo);
+        return getDataTable(tableDataInfos);
+    }
+
+    /**
+     * 导出物料列表
+     */
+    // @PreAuthorize("@ss.hasPermi('system:material-info:export')")
+    @Log(title = "物料资料", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    @ApiOperation(value = "导出物料资料列表", notes = "导出物料资料列表")
+    public void export(HttpServletResponse response, ErpMaterialInfo erpMaterialInfo)
+    {
+        List<ErpMaterialInfo> list = erpMaterialInfoService.selectErpMaterialInfoList(erpMaterialInfo);
+        ExcelUtil<ErpMaterialInfo> util = new ExcelUtil<>(ErpMaterialInfo.class);
+        util.exportExcel(response, list, "物料资料数据");
+    }
+
+    // @PreAuthorize("@ss.hasPermi('system:material-info:import')")
+    @Log(title = "物料资料", businessType = BusinessType.IMPORT)
+    @PostMapping("/import")
+    @ApiOperation(value = "导入物料资料列表", notes = "导入物料资料列表")
+    public AjaxResult importExcel(@RequestPart("file") MultipartFile file) throws IOException {
+        ExcelUtil<ErpMaterialInfo> util = new ExcelUtil<>(ErpMaterialInfo.class);
+        List<ErpMaterialInfo> list = util.importExcel(file.getInputStream());
+        return toAjax(erpMaterialInfoService.insertErpMaterialInfos(list));
+    }
+
+    // @PreAuthorize("@ss.hasPermi('system:material-info:add')")
+    @Anonymous
+    @PostMapping
+    @ApiOperation(value = "新增物料信息", notes = "新增物料信息")
+    public AjaxResult add(@RequestBody ErpMaterialInfo erpMaterialInfo) {
+        return toAjax(erpMaterialInfoService.insertErpMaterialInfo(erpMaterialInfo));
+    }
+
+    // @PreAuthorize("@ss.hasPermi('system:material-info:edit')")
+    @Anonymous
+    @PutMapping
+    @ApiOperation(value = "修改物料信息", notes = "修改物料信息")
+    public AjaxResult edit(@RequestBody ErpMaterialInfo erpMaterialInfo) {
+        return toAjax(erpMaterialInfoService.updateErpMaterialInfo(erpMaterialInfo));
+    }
+
+    // @PreAuthorize("@ss.hasPermi('system:material-info:remove')")
+    @Anonymous
+    @DeleteMapping("/{ids}")
+    @ApiOperation(value = "删除物料信息", notes = "删除物料信息")
+    public AjaxResult remove(@PathVariable Long[] ids) {
+        return toAjax(erpMaterialInfoService.deleteErpMaterialInfoByIds(ids));
+    }
+}
---
+++ buluerp_vue/ruoyi-admin/src/main/java/com/ruoyi/web/mapper/ErpMaterialInfoMapper.java
@@ -0,0 +1,21 @@
+package com.ruoyi.web.mapper;
+
+import com.ruoyi.web.domain.ErpMaterialInfo;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface ErpMaterialInfoMapper {
+    List<ErpMaterialInfo> selectErpMaterialInfoList(ErpMaterialInfo erpMaterialInfo);
+
+    ErpMaterialInfo selectErpMaterialInfoById(Long id);
+
+    int insertErpMaterialInfo(ErpMaterialInfo erpMaterialInfo);
+
+    int updateErpMaterialInfo(ErpMaterialInfo erpMaterialInfo);
+
+    int deleteErpMaterialInfoById(Long id);
+
+    int deleteErpMaterialInfoByIds(Long[] ids);
+}
