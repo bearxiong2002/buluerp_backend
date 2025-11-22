521e2386250716d191afcd16c696cb575c0c094e
vvcoder <mzp.2005@qq.com>
2025-11-22 20:44:35 +0800

    修复缺少Excel注解

--- buluerp_vue/ruoyi-admin/src/main/java/com/ruoyi/web/request/mould/AddMouldRequest.java
+++ buluerp_vue/ruoyi-admin/src/main/java/com/ruoyi/web/request/mould/AddMouldRequest.java
@@ -1,5 +1,6 @@
 package com.ruoyi.web.request.mould;
 
+import com.ruoyi.common.annotation.Excel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.validation.constraints.NotBlank;
@@ -8,10 +9,12 @@ import javax.validation.constraints.NotNull;
 public class AddMouldRequest {
     @ApiModelProperty(value = "模具编号")
     @NotBlank(message = "模具编号不能为空")
+    @Excel(name = "模具编号")
     private String mouldNumber;
 
     @ApiModelProperty(value = "厂商ID")
     @NotNull(message = "厂商ID不能为空")
+    @Excel(name = "生产商ID")
     private Long manufacturerId;
 
     public String getMouldNumber() {
